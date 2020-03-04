c66662b52b66e2bc3bba01337cf60f18bdd308df
root <root@sw-mtx-011.mtx.labs.mlnx>
2020-03-04 16:16:34 -0600

    Fix sum.py
    Signed-off-by: root <root@sw-mtx-011.mtx.labs.mlnx>

--- sum.py
+++ sum.py
@@ -2,5 +2,7 @@ sum = 0
 for i, line in enumerate(open('temp1.txt')):
 	if "SUM" in line:
 		sum = sum + float(line.split()[5])
+	else:
+		sum = sum + float(line.split()[6])
 
 print (sum * 8 / 1000)
