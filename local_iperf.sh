02e99bcc44bb905e7e333403c497a98f696fdd73
root <root@sw-mtx-011.mtx.labs.mlnx>
2020-03-04 15:08:54 -0600

    Set P1
    Signed-off-by: root <root@sw-mtx-011.mtx.labs.mlnx>

--- local_iperf.sh
+++ local_iperf.sh
@@ -2,7 +2,7 @@ set -x #echo on
 
 for i in $(seq 1 $1)
 do
-	taskset $((1<<(($i-1)%24))) iperf3 -B "192.168.$i.64" -c "192.168.$i.65" -t 30 -P8 -p $((5000+$i)) -f M -i 5 &
+	taskset $((1<<(($i-1)%24))) iperf3 -B "192.168.$i.64" -c "192.168.$i.65" -t 30 -P1 -p $((5000+$i)) -f M -i 5 &
 done
 
 wait
