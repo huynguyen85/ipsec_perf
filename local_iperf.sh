3e9691f913c2b820a50314e9f5891a4b366c1a51
root <root@gen-l-vrt-203.mtl.labs.mlnx>
2020-04-18 23:43:11 +0300

    Change the bandwidth limit from unlimited to 10Gbps
    Signed-off-by: root <root@gen-l-vrt-203.mtl.labs.mlnx>

--- local_iperf.sh
+++ local_iperf.sh
@@ -2,7 +2,7 @@ set -x #echo on
 
 for i in $(seq 1 $1)
 do
-	taskset $((1<<(($i-1)%24))) iperf3 -B "192.168.$i.64" -c "192.168.$i.65" -t 300 -P1 -p $((5000+$i)) -f M -i 5 &
+	taskset $((1<<(($i-1)%24))) iperf3 -B "192.168.$i.64" -c "192.168.$i.65" -t 300 -P1 -p $((5000+$i)) -f M -i 5 -b 10000000000 &
 done
 
 wait
