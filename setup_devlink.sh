a6a3dab0dbdcd0f65b213596dc5fab66905143d8
root <root@c-235-10-1-009.mtl.labs.mlnx>
2020-11-16 20:39:53 +0200

    Modify setup_devlink.sh
    Signed-off-by: root <root@c-235-10-1-009.mtl.labs.mlnx>

--- setup_devlink.sh
+++ setup_devlink.sh
@@ -1,84 +1,37 @@
 #!/bin/bash
 set -x #echo on
-IPSEC_MODE=$1
-NUM_VF=$2
-echo 0 > /sys/class/net/enp130s0/device/sriov_numvfs 
-echo $NUM_VF > /sys/class/net/enp130s0/device/sriov_numvfs
-echo none > /sys/class/net/enp130s0/compat/devlink/ipsec_mode
 
-DEVICE=0
-for ((i = 1 ; i <= $NUM_VF ; i++));
-do
-	FUNC=$(( $i % 8 ))
-	if [ $FUNC -eq 0 ]
-	then
-		DEVICE=$(( $DEVICE + 1 ))
-	fi
-	echo 0000:82:0$DEVICE.$FUNC >  /sys/bus/pci/drivers/mlx5_core/unbind
-done
+UPLINK=enp8s0f0
+PCI_BUS=08
 
-echo $IPSEC_MODE > /sys/class/net/enp130s0/compat/devlink/ipsec_mode
-devlink dev eswitch set pci/0000:82:00.0 mode switchdev
+echo 0 > /sys/class/net/$UPLINK/device/sriov_numvfs 
+echo 1 > /sys/class/net/$UPLINK/device/sriov_numvfs
+echo 0000:$PCI_BUS:00.2 > /sys/bus/pci/drivers/mlx5_core/unbind
+echo dmfs > /sys/bus/pci/devices/0000\:$PCI_BUS\:00.0/net/$UPLINK/compat/devlink/steering_mode
+echo full > /sys/class/net/$UPLINK/compat/devlink/ipsec_mode
+devlink dev eswitch set pci/0000:$PCI_BUS:00.0 mode switchdev
+echo 0000:$PCI_BUS:00.2 > /sys/bus/pci/drivers/mlx5_core/bind
 
-DEVICE=0
-for ((i = 1 ; i <= $NUM_VF ; i++));
-do
-	FUNC=$(( $i % 8 ))
-	if [ $FUNC -eq 0 ]
-	then
-		DEVICE=$(( $DEVICE + 1 ))
-	fi
-	echo 0000:82:0$DEVICE.$FUNC >  /sys/bus/pci/drivers/mlx5_core/bind
-done
+ifconfig $UPLINK 192.168.1.64/24 up
+
+REMOTE_SERVER=c-235-10-1-010
 
 #stop openvswitch
 service openvswitch stop
 
-REMOTE_SERVER=exprom-dell2
-ssh $REMOTE_SERVER /bin/bash << EOF
-	#!/bin/bash
-	set -x #echo on
-
-	echo 0 > /sys/class/net/enp130s0f0/device/sriov_numvfs
-	echo $NUM_VF > /sys/class/net/enp130s0f0/device/sriov_numvfs
-EOF
-
-DEVICE=0
-for ((i = 2 ; i <= $NUM_VF+1 ; i++));
-do
-	FUNC=$(( $i % 8 ))
-	if [ $FUNC -eq 0 ]
-	then
-		DEVICE=$(( $DEVICE + 1 ))
-	fi
 ssh $REMOTE_SERVER /bin/bash << EOF
 	set -x #echo on
-	echo 0000:82:0$DEVICE.$FUNC >  /sys/bus/pci/drivers/mlx5_core/unbind
-EOF
-done
 
-ssh $REMOTE_SERVER /bin/bash << EOF
-	echo $IPSEC_MODE > /sys/class/net/enp130s0f0/compat/devlink/ipsec_mode
-	devlink dev eswitch set pci/0000:82:00.0 mode switchdev
-EOF
+	echo 0 > /sys/class/net/$UPLINK/device/sriov_numvfs
+	echo 1 > /sys/class/net/$UPLINK/device/sriov_numvfs
+	echo 0000:$PCI_BUS:00.2 > /sys/bus/pci/drivers/mlx5_core/unbind
+	echo dmfs > /sys/bus/pci/devices/0000\:$PCI_BUS\:00.0/net/$UPLINK/compat/devlink/steering_mode
+	echo full > /sys/class/net/$UPLINK/compat/devlink/ipsec_mode
+	devlink dev eswitch set pci/0000:$PCI_BUS:00.0 mode switchdev
+	echo 0000:$PCI_BUS:00.2 > /sys/bus/pci/drivers/mlx5_core/bind
 
-DEVICE=0
-for ((i = 2 ; i <= $NUM_VF+1 ; i++));
-do
-	FUNC=$(( $i % 8 ))
-	if [ $FUNC -eq 0 ]
-	then
-		DEVICE=$(( $DEVICE + 1 ))
-	fi
-ssh $REMOTE_SERVER /bin/bash << EOF
-	set -x #echo on
-	echo 0000:82:0$DEVICE.$FUNC >  /sys/bus/pci/drivers/mlx5_core/bind
-EOF
-done
-
-ssh $REMOTE_SERVER /bin/bash << EOF
-	set -x #echo on
 	#stop openvswitch
 	service openvswitch stop
-EOF
 
+	ifconfig $UPLINK 192.168.1.65/24 up
+EOF
